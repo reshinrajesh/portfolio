837a963d998726a7a31a5796e40c33469bc46a15
Reshin Rajesh <reshinrajesh2002@gmail.com>
2026-03-05 15:36:20 +0530

    chore: remove unused spotify and openai integrations

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'i.scdn.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'mosaic.scdn.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'wrapped-images.spotifycdn.com',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
